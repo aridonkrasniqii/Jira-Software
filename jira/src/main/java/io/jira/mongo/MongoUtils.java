9a889e61c1106da1be21e3d41468c339c477e389
Aridon Krasniqi <aridonk16@outlook.com>
2023-08-01 19:09:07 +0200

    Implementation of MongoUtils

---
+++ jira/src/main/java/io/jira/mongo/MongoUtils.java
@@ -0,0 +1,83 @@
+package io.jira.mongo;
+
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import io.jira.common.models.BaseEntity;
+import io.jira.common.models.PaginatedEntity;
+import io.jira.common.models.PaginationModel;
+import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
+import io.smallrye.mutiny.Uni;
+import org.bson.conversions.Bson;
+
+
+import java.util.List;
+
+public class MongoUtils {
+    public <T> Uni<T> createEntity(ReactiveMongoCollection<T> collection, T entity) {
+        if (entity instanceof BaseEntity) {
+            ((BaseEntity) entity).setCreatedAndUpdatedAt();
+            ((BaseEntity) entity).setObjectId();
+        }
+
+        return collection.insertOne(entity).onItem().transform(ignored -> entity);
+    }
+
+    public <T> Uni<PaginatedEntity<T>> getEntities(ReactiveMongoCollection<T> collection, PaginationModel paginationModel, Bson filter) {
+        List<Bson> pipeline = List.of(); // TODO: implement aggregation pipeline
+
+        Uni<Long> numberOfDocuments = countDocuments(collection);
+        Uni<List<T>> entities = collection.aggregate(pipeline).collect().asList();
+
+        return Uni.combine()
+            .all()
+            .unis(numberOfDocuments, entities)
+            .combinedWith((numOfDocuments, data) ->
+                paginatedEntity(
+                    data,
+                    numOfDocuments.intValue(),
+                    paginationModel
+                )
+            );
+    }
+
+    public <T> Uni<Long> countDocuments(ReactiveMongoCollection<T> collection) {
+        return collection.countDocuments();
+    }
+
+    public int countPages(int numberOfDocuments, int pageSize) {
+        return (int) Math.ceil((double) numberOfDocuments / pageSize);
+    }
+
+    public <T> PaginatedEntity<T> paginatedEntity(List<T> data, int numberOfDocuments, PaginationModel paginationModel) {
+        return new PaginatedEntity<>(
+            data,
+            numberOfDocuments,
+            paginationModel.getPage(),
+            paginationModel.getLimit(),
+            countPages(numberOfDocuments, PaginationModel.PAGE_SIZE)
+        );
+    }
+
+
+    public <T> Uni<T> getEntity(ReactiveMongoCollection<T> collection, Bson filter) {
+        return collection.find(filter).toUni();
+    }
+
+    public <T> Uni<T> updateEntity(ReactiveMongoCollection<T> collection, Bson filter, T entity) {
+        if (entity instanceof BaseEntity) {
+            ((BaseEntity) entity).setUpdatedAt();
+        }
+
+        return collection.findOneAndReplace(filter, entity, new FindOneAndReplaceOptions().returnDocument(ReturnDocument.AFTER));
+    }
+
+    public <T> Uni<T> updateEntity(ReactiveMongoCollection<T> collection, Bson filter, Bson update) {
+        return collection.findOneAndUpdate(filter, update, new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
+    }
+
+
+    public <T> Uni<T> deleteEntity(ReactiveMongoCollection<T> collection, Bson filter) {
+        return collection.findOneAndDelete(filter);
+    }
+}
