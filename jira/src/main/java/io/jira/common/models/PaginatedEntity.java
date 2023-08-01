6de97ffffd52fdd9559899dc11f14a8dae533739
Aridon Krasniqi <aridonk16@outlook.com>
2023-08-01 19:04:18 +0200

    Implementation of pagination

--- jira/src/main/java/io/jira/common/models/PaginatedEntity.java
+++ jira/src/main/java/io/jira/common/models/PaginatedEntity.java
@@ -8,4 +8,13 @@ public class PaginatedEntity<T> {
     public int page;
     public int limit;
     public int pageCount;
+
+
+    public PaginatedEntity(List<T> data, int itemCount, int page, int limit, int pageCount) {
+        this.data = data;
+        this.itemCount = itemCount;
+        this.page = page;
+        this.limit = limit;
+        this.pageCount = pageCount;
+    }
 }
